777e93b209583962250c8053380ec3e3e5ab22d2
Jose Soares <jose@linux.com>
2013-02-25 18:44:40 -0500

    [-] Added a bunch of front-end js
    - angularjs
      - angular-markdown
      - angular-sanitize
    - showdown
      - extensions included

---
+++ public/js/extensions/github.js
@@ -0,0 +1,33 @@
+//
+//  Github Extension (WIP)
+//  ~~strike-through~~   ->  <del>strike-through</del>
+//
+
+(function(){
+    var github = function(converter) {
+        return [
+            {
+              // strike-through
+              // NOTE: showdown already replaced "~" with "~T", so we need to adjust accordingly.
+              type    : 'lang',
+              regex   : '(~T){2}([^~]+)(~T){2}',
+              replace : function(match, prefix, content, suffix) {
+                  return '<del>' + content + '</del>';
+              }
+            },
+            {
+              // @mentions
+              type    : 'lang',
+              regex   : '(@([A-Za-z0-9-_]+))',
+              replace : function(match, content, user) {
+                  return '<a href="//github.com/' + user + '">' + content + '</a>'
+              }
+            }
+        ];
+    };
+
+    // Client-side export
+    if (typeof window !== 'undefined' && window.Showdown && window.Showdown.extensions) { window.Showdown.extensions.github = github; }
+    // Server-side export
+    if (typeof module !== 'undefined') module.exports = github;
+}());
